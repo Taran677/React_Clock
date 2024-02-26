3054793d4c632a64efc1a33e829840f3fc580b99
Taranver Singh <taranveersinghbanga@gmail.com>
2024-02-26 21:59:06 +0530

    Your commit message

--- src/components/Clock_Analog.jsx
+++ src/components/Clock_Analog.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-import {ClockRun} from "./ClockRunner";
-ClockRun()
+import { ClockRun } from "./ClockRunner";
+ClockRun();
 function Clock_Analog() {
   return (
     <div className="container1">
       <div className="circle">
         <div className="circleSmall">
           <div className="circleCenter">
-            <img className="handle1" src="../../src\assets\ClockHandle.png" alt="3" />
+            <img
+              className="handle1"
+              src="..\..\src\assets\ClockHandle.png"
+              alt="3"
+            />
             <img
               className="handle1 handle2"
-              src="../../src\assets\ClockHandle2.png"
+              src="..\..\src\assets\ClockHandle2.png"
               alt="2"
             />
             <img
               className="handle1 handle3"
-              src="../../src\assets\ClockHandle3.png"
+              src="..\..\src\assets\ClockHandle3.png"
               alt="1"
             />
           </div>
